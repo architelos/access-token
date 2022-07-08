bbda24ec7bad6fbf078293169322c22ce1e5e4f6
architelos <108888162+architelos@users.noreply.github.com>
2022-07-08 18:55:01 +0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,5 +1,5 @@
 const token = require('./core.js')
 
 token(function(accessToken) {
-    console.log(accessToken); // access token is now stored in accessToken
+    console.log(accessToken); // access token is now stored in accessToken // If there was an error, accessToken will be "Could not generate access token."
 }
