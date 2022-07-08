7462d597777fe56eeaaaffdf9f76d6f7ca92b1b5
architelos <108888162+architelos@users.noreply.github.com>
2022-07-08 18:55:57 +0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -2,5 +2,5 @@ const token = require('./core.js')
 
 token(function(accessToken) {
     console.log(accessToken); // access token is now stored in accessToken // If there was an error, accessToken will be "Could not generate access token."
-    // Remember that an access token only lasts 12 hours and can only  be generated 4-5 times per day.
+    // Remember that an access token only lasts 12 hours and can only be generated 4-5 times per day.
 }
